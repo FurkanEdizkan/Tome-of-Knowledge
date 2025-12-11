47b560848f101182f60f3cd97cfce6f8aa23ca6f
FurkanEdizkan <furkandedizkan@gmail.com>
2025-12-11 15:20:52 +0300

    Title: Fixed a wrong number issue Issues: Co-workers: Details:

--- Tutorials/LeetCode/Solutions/17-Letter_Combinations_of_a_Phone_Number.py
+++ Tutorials/LeetCode/Solutions/17-Letter_Combinations_of_a_Phone_Number.py
@@ -9,7 +9,7 @@ class Solution:
             "3": "def",
             "4": "ghi",
             "5": "jkl",
-            "6": "mnp",
+            "6": "mno",
             "7": "pqrs",
             "8": "tuv",
             "9": "wxyz"
