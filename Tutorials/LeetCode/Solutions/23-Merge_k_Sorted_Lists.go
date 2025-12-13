d4b3bbcb81434f5e0b9f14d2c6b88448b8bfc3ff
FurkanEdizkan <furkandedizkan@gmail.com>
2025-12-13 11:56:18 +0000

    Solved question 23

---
+++ Tutorials/LeetCode/Solutions/23-Merge_k_Sorted_Lists.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+    "container/heap"
+    "fmt"
+)
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+type MinHeap []*ListNode
+
+func (h MinHeap) Len() int           { return len(h) }
+func (h MinHeap) Less(i, j int) bool { return h[i].Val < h[j].Val }
+func (h MinHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *MinHeap) Push(x interface{}) {
+    *h = append(*h, x.(*ListNode))
+}
+
+func (h *MinHeap) Pop() interface{} {
+    old := *h
+    n := len(old)
+    x := old[n-1]
+    *h = old[0 : n-1]
+    return x
+}
+
+func mergeKLists(lists []*ListNode) *ListNode {
+    if len(lists) == 0 {
+        return nil
+    }
+
+    h := &MinHeap{}
+    heap.Init(h)
+
+    // Push the head of each non-empty list
+    for _, list := range lists {
+        if list != nil {
+            heap.Push(h, list)
+        }
+    }
+
+    dummy := &ListNode{}
+    curr := dummy
+
+    for h.Len() > 0 {
+        minNode := heap.Pop(h).(*ListNode)
+        curr.Next = minNode
+        curr = curr.Next
+
+        if minNode.Next != nil {
+            heap.Push(h, minNode.Next)
+        }
+    }
+
+    return dummy.Next
+}
