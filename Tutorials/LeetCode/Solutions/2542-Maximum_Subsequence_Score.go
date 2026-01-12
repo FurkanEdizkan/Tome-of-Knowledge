a5b05d097b9edd9c176f2b9144800691465eef8c
FurkanEdizkan <furkandedizkan@gmail.com>
2026-01-12 10:09:41 +0000

    Refactor maxScore function for improved readability and performance

--- Tutorials/LeetCode/Solutions/2542-Maximum_Subsequence_Score.go
+++ Tutorials/LeetCode/Solutions/2542-Maximum_Subsequence_Score.go
@@ -1,3 +1,5 @@
+import "sort"
+
 func maxScore(nums1 []int, nums2 []int, k int) int64 {
 	n := len(nums1)
 
@@ -6,17 +8,13 @@ func maxScore(nums1 []int, nums2 []int, k int) int64 {
 		pairs[i] = [2]int{nums2[i], nums1[i]}
 	}
 
-	for i := 0; i < n; i++ {
-		for j := i + 1; j < n; j++ {
-			if pairs[j][0] > pairs[i][0] {
-				pairs[i], pairs[j] = pairs[j], pairs[i]
-			}
-		}
-	}
+	sort.Slice(pairs, func(i, j int) bool {
+		return pairs[i][0] > pairs[j][0]
+	})
 
 	heap := make([]int, 0)
 	var sum int64 = 0
-	var result int64 = 0
+	var ans int64 = 0
 
 	push := func(x int) {
 		heap = append(heap, x)
@@ -32,7 +30,7 @@ func maxScore(nums1 []int, nums2 []int, k int) int64 {
 	}
 
 	pop := func() int {
-		top := heap[0]
+		res := heap[0]
 		last := heap[len(heap)-1]
 		heap = heap[:len(heap)-1]
 		if len(heap) > 0 {
@@ -42,7 +40,6 @@ func maxScore(nums1 []int, nums2 []int, k int) int64 {
 				l := 2*i + 1
 				r := 2*i + 2
 				smallest := i
-
 				if l < len(heap) && heap[l] < heap[smallest] {
 					smallest = l
 				}
@@ -56,7 +53,7 @@ func maxScore(nums1 []int, nums2 []int, k int) int64 {
 				i = smallest
 			}
 		}
-		return top
+		return res
 	}
 
 	for _, p := range pairs {
@@ -69,11 +66,11 @@ func maxScore(nums1 []int, nums2 []int, k int) int64 {
 
 		if len(heap) == k {
 			score := sum * int64(p[0])
-			if score > result {
-				result = score
+			if score > ans {
+				ans = score
 			}
 		}
 	}
 
-	return result
+	return ans
 }
