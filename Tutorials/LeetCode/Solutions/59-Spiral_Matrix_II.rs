4a4a41effd4ae6fd263ed0e4f83516f016f690f6
FurkanEdizkan <furkandedizkan@gmail.com>
2026-04-24 08:53:05 +0300

    Add problem description and solution for "59. Spiral Matrix II"

---
+++ Tutorials/LeetCode/Solutions/59-Spiral_Matrix_II.rs
@@ -0,0 +1,43 @@
+impl Solution {
+    pub fn generate_matrix(n: i32) -> Vec<Vec<i32>> {
+        let n = n as usize;
+        let mut matrix = vec![vec![0; n]; n];
+
+        let (mut top, mut bottom) = (0, n as i32 - 1);
+        let (mut left, mut right) = (0, n as i32 - 1);
+
+        let mut num = 1;
+
+        while top <= bottom && left <= right {
+
+            for col in left..=right {
+                matrix[top as usize][col as usize] = num;
+                num += 1;
+            }
+            top += 1;
+
+            for row in top..=bottom {
+                matrix[row as usize][right as usize] = num;
+                num += 1;
+            }
+            right -= 1;
+
+            if top <= bottom {
+                for col in (left..=right).rev() {
+                    matrix[bottom as usize][col as usize] = num;
+                    num += 1;
+                }
+                bottom -= 1;
+            }
+
+            if left <= right {
+                for row in (top..=bottom).rev() {
+                    matrix[row as usize][left as usize] = num;
+                    num += 1;
+                }
+                left += 1;
+            }
+        }
+        matrix
+    }
+}
